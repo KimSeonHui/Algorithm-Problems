ee2dc96ddc911426ed369ad7c4ec437340939255
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-05-17 16:16:45 +0900

    Time: 106 ms (36.67%), Space: 45.1 MB (48.84%) - LeetHub

--- LeetCode/141-linked-list-cycle/141-linked-list-cycle.js
+++ LeetCode/141-linked-list-cycle/141-linked-list-cycle.js
@@ -10,18 +10,35 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// two pointers
 var hasCycle = function(head) {
-    const set = new Set();
-    let currentNode = head;
+    let slow = head;
+    let fast = head;
     
-    while(currentNode !== null && currentNode.next !== null) {
-        if(!set.has(currentNode)) {
-            set.add(currentNode);
-        } 
-        else return true;
+    while(fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
         
-        currentNode = currentNode.next;
+        if(slow === fast) return true;
     }
     
     return false;
 };
+
+
+// set
+// var hasCycle = function(head) {
+//     const set = new Set();
+//     let currentNode = head;
+    
+//     while(currentNode !== null && currentNode.next !== null) {
+//         if(!set.has(currentNode)) {
+//             set.add(currentNode);
+//         } 
+//         else return true;
+        
+//         currentNode = currentNode.next;
+//     }
+    
+//     return false;
+// };
