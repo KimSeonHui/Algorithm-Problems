f7e375dc1199bbe5da5904d73d928e6bdee85f7f
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-05-17 16:06:29 +0900

    Time: 108 ms (34.26%), Space: 46 MB (9.03%) - LeetHub

---
+++ LeetCode/141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    const set = new Set();
+    let currentNode = head;
+    
+    while(currentNode !== null && currentNode.next !== null) {
+        if(!set.has(currentNode)) {
+            set.add(currentNode);
+        } 
+        else return true;
+        
+        currentNode = currentNode.next;
+    }
+    
+    return false;
+};
