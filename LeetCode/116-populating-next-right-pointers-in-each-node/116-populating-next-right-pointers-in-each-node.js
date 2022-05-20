96b500e597e6df15de1265059412671a78dd8967
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-05-20 18:00:16 +0900

    Update 116-populating-next-right-pointers-in-each-node.js

--- LeetCode/116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
+++ LeetCode/116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
@@ -32,3 +32,19 @@ function setNextPointer(parentNode, root, subTree) {
     
     return root;
 }
+
+//다른 사람 풀이
+var connect = function(root) {
+    let ptr = root;
+    while(root && root.left){
+        let temp = root;
+        while(temp) {
+            temp.left.next = temp.right;
+            temp.right.next = temp.next ? temp.next.left : null;
+            temp = temp.next;
+        }
+        root = root.left;
+    }
+    return ptr;
+}
+
