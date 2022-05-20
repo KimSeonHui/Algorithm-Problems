09b2299c2dda351ee4c90939647c38566914ca6b
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-05-20 19:32:16 +0900

    Time: 109 ms (25.18%), Space: 44 MB (74.52%) - LeetHub

--- LeetCode/21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ LeetCode/21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,21 +11,68 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if(list1 === null) return list2;
-    else if(list2 === null) return list1;
+    if(!list1) return list2;
+    if(!list2) return list1;
     
-    while(list1 !== null && list2 !== null) {
-        if(list1.val < list2.val) {
-            list1.next =  mergeTwoLists(list1.next, list2);
+    let l1 = list1;
+    let l2 = list2;
+    
+    while(l1 !== null && l2 !== null) {
+        if(l1.val < l2.val) {
+            const temp = l1.next;
+            if(temp !== null) l1.next = (temp.val < l2.val) ? temp : l2;
+            else l1.next = l2;
             
-            return list1;
+            l1 = temp;
         }
         else {
-            list2.next =  mergeTwoLists(list1, list2.next);
-            
-            return list2;
+            const temp = l2.next;
+            if(temp !== null) l2.next = (temp.val <= l1.val) ? temp : l1;
+            else l2.next = l1;
+
+            l2 = temp;
         }
     }
     
-   
+   return list1.val < list2.val ? list1 : list2;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var mergeTwoLists = function(list1, list2) {
+//     if(list1 === null) return list2;
+//     else if(list2 === null) return list1;
+    
+//     while(list1 !== null && list2 !== null) {
+//         if(list1.val < list2.val) {
+//             list1.next =  mergeTwoLists(list1.next, list2);
+            
+//             return list1;
+//         }
+//         else {
+//             list2.next =  mergeTwoLists(list1, list2.next);
+            
+//             return list2;
+//         }
+//     }
+    
+   
+// };
