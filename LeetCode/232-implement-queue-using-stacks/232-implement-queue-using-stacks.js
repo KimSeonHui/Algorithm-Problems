1652aeff68c283ff8a46bb0540575e1b774075f2
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-05-23 16:06:27 +0900

    Time: 68 ms (68.35%), Space: 42.4 MB (12.84%) - LeetHub

---
+++ LeetCode/232-implement-queue-using-stacks/232-implement-queue-using-stacks.js
@@ -0,0 +1,59 @@
+
+var MyQueue = function() {
+    this.front = [];
+    this.rear = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    if(this.front.length === 0) this.front.push(x);
+    else this.rear.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if(this.front.length > 0 ) {
+        const ele = this.front.pop();
+        
+        if(this.front.length === 0 && this.rear.length > 0) {
+            this.front.push(this.rear[0]);
+            
+            for(let i = 0; i < this.rear.length; i++) {
+                if(i !== this.rear.length - 1) this.rear[i] = this.rear[i+1];
+                else this.rear.pop();
+            }
+        }
+        
+        return ele;
+    }
+    else return undefined;
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    if(this.front.length > 0) return this.front[0];
+    else return undefined;
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return (this.front.length === 0 && this.rear.length === 0);
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
