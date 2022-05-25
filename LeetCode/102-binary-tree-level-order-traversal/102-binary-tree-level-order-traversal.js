6a7120413d0eeb6d396f3f67c072b5a5eee4544d
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-05-25 16:29:59 +0900

    Time: 140 ms (5.20%), Space: 44.2 MB (71.53%) - LeetHub

--- LeetCode/102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ LeetCode/102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -10,29 +10,58 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+//otehr solution
 var levelOrder = function(root) {
     if(!root) return [];
     
     const order = [];
-    const queue = [];
-    let levelArr = [];
-    let prevNode = {'root' : root, 'level' : 0};
-    queue.push({'root' : root, 'level' : 0});
-    
+    const queue = [root];
+
     while(queue.length > 0) {
-        const node = queue.shift();
-        if(prevNode.level !== node.level) {
-            order.push(levelArr);
-            levelArr = [];
+        const row = [];
+        const length = queue.length;
+        for(let i = 0; i < length; i++) {
+            const node = queue.shift();
+            row.push(node.val);
+            
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
         }
-        
-        if(node.root.left) queue.push({'root' : node.root.left, 'level' : node.level + 1});
-        if(node.root.right) queue.push({'root' : node.root.right, 'level' : node.level + 1});
-        levelArr.push(node.root.val);
-        
-        prevNode = node;
+        order.push(row);
     }
     
-    order.push(levelArr);   
+ 
     return order;
 };
+
+
+
+
+
+
+// var levelOrder = function(root) {
+//     if(!root) return [];
+    
+//     const order = [];
+//     const queue = [];
+//     let levelArr = [];
+//     let prevNode = {'root' : root, 'level' : 0};
+//     queue.push({'root' : root, 'level' : 0});
+    
+//     while(queue.length > 0) {
+//         const node = queue.shift();
+//         if(prevNode.level !== node.level) {
+//             order.push(levelArr);
+//             levelArr = [];
+//         }
+        
+//         if(node.root.left) queue.push({'root' : node.root.left, 'level' : node.level + 1});
+//         if(node.root.right) queue.push({'root' : node.root.right, 'level' : node.level + 1});
+//         levelArr.push(node.root.val);
+        
+//         prevNode = node;
+//     }
+    
+//     order.push(levelArr);   
+//     return order;
+// };
