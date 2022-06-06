be2ebc13a41ec7634571cb2687d621294f43ff01
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-06-06 15:46:00 +0900

    Time: 385 ms (20.26%), Space: 61.9 MB (18.32%) - LeetHub

---
+++ LeetCode/542-01-matrix/542-01-matrix.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function(mat) {
+    const queue = [];
+    const dis = new Array(mat.length).fill().map(() => Array(mat[0].length));
+    const direction = [[-1,0], [1,0], [0,-1], [0,1]];
+
+    for(let i = 0; i < mat.length; i++) {
+        for(let j = 0; j < mat[i].length; j++) {
+            if(mat[i][j] === 0) {
+                dis[i][j] = 0;
+                queue.push([i, j]);
+            }
+            else dis[i][j] = -1;
+        }
+    }
+    
+    while(queue.length > 0) {
+        const [row, col] = queue.shift();
+        
+        for(let [dx, dy] of direction) {
+            const r = row + dx;
+            const c = col + dy;
+            
+            if(r < 0 || r >= mat.length || c < 0 || c >= mat[0].length) continue;
+            
+            if(dis[r][c] === 0) continue;
+            if(dis[r][c] === -1) {
+                dis[r][c] = dis[row][col] + 1;
+                queue.push([r,c]);
+            }
+        }
+    }
+
+    return dis;
+};
+
+
