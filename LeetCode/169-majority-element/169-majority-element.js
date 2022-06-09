adfbcb85fd6d07bfee15ec52621f6e78c396ca20
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-06-09 20:35:09 +0900

    Time: 80 ms (70.86%), Space: 43.8 MB (55.92%) - LeetHub

--- LeetCode/169-majority-element/169-majority-element.js
+++ LeetCode/169-majority-element/169-majority-element.js
@@ -8,6 +8,6 @@ var majorityElement = function(nums) {
     for(let num of nums) {
         hash[num] = (hash[num] | 0) + 1;
         
-        if(hash[num] > Math.floor(nums.length / 2)) return num;
+        if(hash[num] >nums.length / 2) return num;
     }
 };
