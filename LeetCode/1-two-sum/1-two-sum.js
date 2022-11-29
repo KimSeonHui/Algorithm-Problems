cb695bc65d3eb917121c5366177cfa1891b223c4
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-11-29 12:09:42 +0900

    Time: 103 ms (77.84%), Space: 43.1 MB (32.14%) - LeetHub

--- LeetCode/1-two-sum/1-two-sum.js
+++ LeetCode/1-two-sum/1-two-sum.js
@@ -3,19 +3,35 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {    
-    const answer = [];
+var twoSum = function(nums, target) {  
+    // 풀이 1
+//     const answer = [];
     
+//     for(let i = 0; i < nums.length; i++) {
+//         let rest = target - nums[i];
+//         if(nums.includes(rest)) {
+//             let restIndex = nums.indexOf(rest);
+//             if(restIndex !== i) {
+//                 answer.push(i);
+//                 answer.push(restIndex);
+//                 break;
+//             }
+//         }
+//     }
+//     return answer;
+    
+    
+    //풀이 2
+    const hash = {};  
     for(let i = 0; i < nums.length; i++) {
-        let rest = target - nums[i];
-        if(nums.includes(rest)) {
-            let restIndex = nums.indexOf(rest);
-            if(restIndex !== i) {
-                answer.push(i);
-                answer.push(restIndex);
-                break;
-            }
+        let cur = nums[i];
+        let rest = target-cur;
+        if(hash[rest] !== undefined) {
+            return [hash[rest], i];
+        }
+        else {
+            hash[cur] = i;
         }
     }
-    return answer;
+    
 };
