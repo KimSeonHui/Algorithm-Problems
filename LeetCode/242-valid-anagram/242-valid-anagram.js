091b90dd53c3f04474eb2a2fcd4d1c17c4b6edaa
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-12-03 14:57:50 +0900

    Time: 181 ms (17.80%), Space: 49 MB (12.40%) - LeetHub

--- LeetCode/242-valid-anagram/242-valid-anagram.js
+++ LeetCode/242-valid-anagram/242-valid-anagram.js
@@ -6,17 +6,9 @@
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
     
-    const arr1 = [];
-    const arr2 = [];
+    s = s.split('').sort();
+    t = t.split('').sort();
     
-    for(let i = 0; i < s.length; i++) {
-        arr1.push(s[i]);
-        arr2.push(t[i]);
-    }
-    
-    arr1.sort();
-    arr2.sort();
-    
-    if(arr1.toString() === arr2.toString()) return true;
+    if(s.toString() === t.toString()) return true;
     else return false;
 };
