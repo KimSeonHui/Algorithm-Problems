17294569e0ae89a31be0547fb1d03ea79030682a
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2022-12-08 14:45:41 +0900

    Time: 75 ms (90.7%), Space: 43.9 MB (96.56%) - LeetHub

--- LeetCode/101-symmetric-tree/101-symmetric-tree.js
+++ LeetCode/101-symmetric-tree/101-symmetric-tree.js
@@ -10,13 +10,36 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function(root) {    
-    const checkSymmetric = (left, right) => {
-        if(left === null && right === null) return true;
-        if(left === null || right === null)  return false;
-        if(left.val !== right.val) return false;
-        
-        return checkSymmetric(left.left, right.right) && checkSymmetric(left.right, right.left);
-    }
-    return checkSymmetric(root.left, root.right);
+var isSymmetric = function(root) {
+    const left = getLeftTree(root.left, []);
+    const right = getRightTree(root.right, []);
+
+    if(JSON.stringify(left) === JSON.stringify(right)) return true;
+    else return false;
 };
+
+const getLeftTree = (root, leftTree) => {
+    if(!root) return [];
+
+    leftTree.push(root.val);
+
+    if(root.left) leftTree = getLeftTree(root.left, leftTree);
+    if(!root.left) leftTree.push(null);
+    if(root.right) leftTree = getLeftTree(root.right, leftTree);
+    if(!root.right) leftTree.push(null);
+
+    return leftTree;
+}
+
+const getRightTree = (root, rightTree) => {
+    if(!root) return [];
+
+    rightTree.push(root.val);
+
+    if(root.right) rightTree = getRightTree(root.right, rightTree);
+    if(!root.right) rightTree.push(null);
+    if(root.left) rightTree = getRightTree(root.left, rightTree);
+    if(!root.left) rightTree.push(null);
+
+    return rightTree;
+}
