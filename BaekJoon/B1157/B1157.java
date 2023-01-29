e1d458a46c764e2e2430ca04981b991e68e35de1
KimSeonHui <zero9657@naver.com>
2023-01-29 10:54:48 +0900

    add B1157 단어 공부 another solution

--- BaekJoon/B1157/B1157.java
+++ BaekJoon/B1157/B1157.java
@@ -1,36 +1,31 @@
 package backjoon;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class B1157 {
-	static int max = 0;
-	static char answer = '0';
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		HashMap<Character, Integer> map = new HashMap<>();
+		String str = br.readLine();
 		
-		String str = sc.next();
-		
-		for(int i = 0; i < str.length(); i++) {
+		for(int i=0; i <str.length(); i++) {
 			char key = Character.toUpperCase(str.charAt(i));
 			if(!map.containsKey(key)) map.put(key, 1);
 			else map.put(key, map.get(key)+1);
 		}
 		
-		
-		map.forEach((key, value) -> {
-			if(max < value) {
-				max = value;
-				answer = key;
+		int max = 0;
+		char answer = ' ';
+		for(Map.Entry<Character, Integer> entry : map.entrySet()) {
+			if(max < entry.getValue()) {
+				max = entry.getValue();
+				answer = entry.getKey();
 			}
-			else if(max == value) {
-				max = value;
+			else if(max == entry.getValue()) {
 				answer = '?';
 			}
-		});
-		
+		}
 		System.out.println(answer);
 	}
 
