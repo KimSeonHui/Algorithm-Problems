242350ae3ea60b8d823031267586199e99797212
KimSeonHui <zero9657@naver.com>
2023-02-03 21:24:09 +0900

    add B1475 방번호 another solution

--- BaekJoon/B1475/B1475.java
+++ BaekJoon/B1475/B1475.java
@@ -3,35 +3,23 @@ package backjoon;
 import java.io.*;
 
 public class B1475 {
-	static int[] nums = {1,1,1,1,1,1,1,1,1,1};
-    static int cnt = 1;
-    static int index = 0;
-    
 	public static void main(String[] args) throws IOException {
 		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	     String str = br.readLine();
+	     int[] nums = new int[9];
+		 int cnt = 0;
 
 	     for(int i=0; i<str.length(); i++) {
-	    	 index = str.charAt(i) - '0';
-	    	 if(index != 9 && index != 6) {
-	    		 if(nums[index] > 0) nums[index]--;
-	    		 else  addSet();
-	    	 }
-	    	 else {
-	    		 if(nums[9] > 0) nums[9]--;
-	    		 else if(nums[6] > 0) nums[6]--;
-	    		 else  addSet();
-	    	 }
+	    	 int index = str.charAt(i) - '0';
+	    	 if(index == 9) nums[6]++;
+	    	 else nums[index]++;
 	     }
+	     
+	     nums[6] = (nums[6] % 2 == 0) ? nums[6]/2 : nums[6]/2+1;
+	     for(int num : nums) {
+	    	 cnt = Math.max(cnt, num);
+	     }
+	     
 	     System.out.println(cnt);
 	}
-	
-	public static void addSet() {
-		cnt++;
-		for(int i=0; i < nums.length; i++) {
-			if(index != i) nums[i]++;
-		}
-	}
-	
-
 }
