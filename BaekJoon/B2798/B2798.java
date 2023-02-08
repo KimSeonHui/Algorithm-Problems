afdf29160e085c29096829069184282b9c167cde
KimSeonHui <zero9657@naver.com>
2023-02-08 21:54:09 +0900

    add B2798 블랙잭 another solution

--- BaekJoon/B2798/B2798.java
+++ BaekJoon/B2798/B2798.java
@@ -4,30 +4,46 @@ import java.io.*;
 import java.util.*;
 
 public class B2798 {
-
+	static int N;
+	static int M;
+	static int min = 0;
+	static int[] cards;
+	static int[] combs;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] cards = new int[N];
-		int sum = 0;
-		int min = 0;
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		cards = new int[N];
+		combs = new int[N];
+		min = 0;
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
 			cards[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i=0; i<N; i++) {
-			for(int j=i+1; j<N; j++) {
-				for(int k=j+1; k<N; k++) {
-					sum = cards[i] + cards[j] + cards[k];
-					if(sum <= M) min = M-sum < M-min ? sum : min;
-				}
-			}
-		}
+		comb(0,0);
 		System.out.println(min);
 	}
-
+	
+	static void comb(int cnt, int start) {
+		if(cnt == 3) {
+			// 합 계산
+			int sum = 0;
+			for(int i=0; i<3; i++) {
+				sum += combs[i];
+			}
+			if(sum <= M) { // 3 카드의 합이 M보다 작거나 같으면 합 중에서 최소값 계산
+				min = Math.max(sum, min);
+			}
+			return;
+		}
+		
+		for(int i=start; i<N; i++) {
+			combs[cnt] = cards[i];
+			comb(cnt+1, i+1);
+		}
+	}
 }
