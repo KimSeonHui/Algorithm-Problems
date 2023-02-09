b14ea61ec12940362eab915c72570a30ed625f8f
KimSeonHui <zero9657@naver.com>
2023-02-09 20:01:30 +0900

    add B2961 도영이가 만든 맛있는 음식  another solution

--- BaekJoon/B2961/B2961.java
+++ BaekJoon/B2961/B2961.java
@@ -21,28 +21,20 @@ public class B2961 {
 		}
 		
 
-		subset(0);
+		subset(0, 1, 0);
 		System.out.println(min);
 	}
-	static void subset(int cnt) {
+	static void subset(int cnt, int f1, int f2) {
 		if(cnt == N) {
-			int f1 = 1; // 신맛
-			int f2 = 0; // 쓴맛
-			for(int i=0; i<N; i++) {
-				if(v[i]) {
-					f1 *= flavors[i][0];
-					f2 += flavors[i][1];
-				}
-			}
             if(f1 == 1 && f2 == 0) return; // 공집합일 때(아무것도 선택 안 됐을 때) 처리
 			min = Math.min(min, Math.abs(f1-f2));
 			return;
 		}
 		
 		v[cnt] = true;
-		subset(cnt+1);
+		subset(cnt+1, f1*flavors[cnt][0], f2 +flavors[cnt][1]);
 		v[cnt] = false;
-		subset(cnt+1);
+		subset(cnt+1, f1, f2);
 	}
 
 }
