36b4c8b110c5879b9e3d5fcd90c5c4c27e98ca9a
KimSeonHui <zero9657@naver.com>
2023-02-25 21:11:15 +0900

    add N과 M 10

---
+++ BaekJoon/B15664 N과 M 10/B15664.java
@@ -0,0 +1,43 @@
+package M02_4;
+
+import java.io.*;
+import java.util.*;
+
+public class B15664 {
+	static StringBuilder sb;
+	static int N, M;
+	static int[] res, input;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		res = new int[M];
+		input = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<N; i++) input[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(input);
+		comb(0, 0);
+		System.out.println(sb.toString());
+		br.close();
+	}
+	
+	static void comb(int cnt, int start) {
+		if(cnt == M) {
+			for(int i=0; i<M; i++) sb.append(res[i]).append(" ");
+			sb.append("\n");
+			return;
+		}
+		int checked = -1;
+		for(int i=start; i<N; i++) {
+			if(checked == input[i]) continue;
+			res[cnt] = input[i];
+			checked = input[i];
+			comb(cnt+1, i+1);
+		}
+	}
+}
