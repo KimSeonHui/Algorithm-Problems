103aa36a6b42cea71c9517fe77e38c5218a10795
KimSeonHui <zero9657@naver.com>
2023-02-25 20:49:02 +0900

    add B15655 N과M 6

---
+++ BaekJoon/B15655 N과 M 6/B15655.java
@@ -0,0 +1,41 @@
+package M02_4;
+
+import java.io.*;
+import java.util.*;
+
+public class B15655 {
+
+	static int N, M;
+	static int[] input, res;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		input = new int[N];
+		res = new int[M];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input);
+		comb(0, 0);
+		System.out.println(sb.toString());
+	}
+	
+	static void comb(int cnt, int start) {
+		if(cnt == M) {
+			for(int i=0; i<M; i++) sb.append(res[i]).append(" ");
+			sb.append("\n");
+			return;
+		}
+		for(int i=start; i<N; i++) {
+			res[cnt] = input[i];
+			comb(cnt+1, i+1);
+		}
+	}
+}
