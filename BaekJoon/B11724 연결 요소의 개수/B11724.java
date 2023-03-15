e7cf6a66cdeb22b4b15c3edcbeb440e34765d611
KimSeonHui <zero9657@naver.com>
2023-03-15 21:41:35 +0900

    add B11724 연결 요소의 개수 수정

--- BaekJoon/B11724 연결 요소의 개수/B11724.java
+++ BaekJoon/B11724 연결 요소의 개수/B11724.java
@@ -4,35 +4,25 @@ import java.io.*;
 import java.util.*;
 
 public class B11724 {
-	static List<Integer>[] g;
-	static boolean[] v;
+	static int[] p;
+	static int N;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
+		int cnt = N;
 		
-		g = new List[N+1];
-		for(int i=0; i<=N; i++) {
-			g[i] = new ArrayList<>();
-		}
+		makeSet();
 		
 		for(int i=0; i<E; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			
-			g[from].add(to);
-			g[to].add(from);
-		}
-		
-		v = new boolean[N+1];
-		int cnt = 0;
-		for(int i=1; i<=N; i++) {
-			if(!v[i]) {
-				bfs(i);
-				cnt++;
+			if(union(from, to)) {
+				cnt--;
 			}
 		}
 		
@@ -40,21 +30,23 @@ public class B11724 {
 		br.close();
 	}
 	
-	static void bfs(int num) {
-		ArrayDeque<Integer> q = new ArrayDeque<>();
-		v[num] = true;
-		q.offer(num);
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			
-			for(int vertex : g[cur]) {
-				if(!v[vertex]) {
-					v[vertex] = true;
-					q.offer(vertex);
-				}
-			}
-		}
+	static void makeSet() {
+		p = new int[N+1];
+		for(int i=1; i<=N; i++) p[i] = i;
 	}
-
+	
+	static int find(int a) {
+		if(a == p[a]) return a;
+		return p[a] = find(p[a]);
+	}
+	
+	static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if(aRoot == bRoot) return false;
+		p[bRoot] = aRoot;
+		
+		return true;
+	}
 }
