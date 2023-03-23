2c90e8e4d15bb7630a4bd930435df7c7ff8d9df7
KimSeonHui <zero9657@naver.com>
2023-03-23 19:13:07 +0900

    add B11726 2xn 타일링

---
+++ BaekJoon/B11726 2xn 타일링/B11726.java
@@ -0,0 +1,30 @@
+package M03_4;
+
+import java.io.*;
+
+public class B11726 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[N+1];
+		if(N==1) {
+			System.out.println(1);
+			System.exit(0);
+		}
+		else if(N==2) {
+			System.out.println(2);
+			System.exit(0);
+		}
+		else {
+			dp[1] = 1;
+			dp[2] = 2;
+			for(int i=3; i<=N; i++) {
+				dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+			}
+		}
+		System.out.println(dp[N]);
+		br.close();
+	}
+}
