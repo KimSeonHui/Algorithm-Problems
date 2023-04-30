fed025724dfea24fbd268371e5cedf3303bc79b6
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2023-04-30 12:11:15 +0900

    [Gold IV] Title: 괄호, Time: 524 ms, Memory: 14832 KB -BaekjoonHub

--- 백준/Gold/10422. 괄호/괄호.java
+++ 백준/Gold/10422. 괄호/괄호.java
@@ -10,19 +10,18 @@ public class Main {
 		for(int t=0; t<T; t++) {
 			int N = Integer.parseInt(br.readLine());
 			
-			long[] dp = new long[N+1]; // 카탈란 수 :: 괄호의 쌍의 개수가 i일때 올바른 괄호 문자열의 개수 
+			long[] dp = new long[N+1]; // 문자열의 길이가 i일때 올바른 괄호 문자열의 개수 
 			dp[0] = 1;
-			for(int i=0; i<N; i++) { // n
-				for(int j=0; j<=i; j++) {
-					dp[i+1] += dp[j]*dp[i-j];
-                    dp[i+1] %= 1000000007;
+			if(N >=2) {
+				dp[2] = 1;
+				for(int i=2; i<=N/2; i++) {
+					for(int j=0; j<=i-1; j++) {
+						dp[i*2] += dp[j*2]*dp[(i-1-j)*2];
+						dp[i*2] %= 1000000007;
+					}
 				}
 			}
-			
-			if(N % 2 ==0) {
-				sb.append(dp[N/2]).append("\n");
-			}
-			else sb.append(0).append("\n");
+			sb.append(dp[N]).append("\n");
 		}
 		System.out.println(sb);
 		br.close();
