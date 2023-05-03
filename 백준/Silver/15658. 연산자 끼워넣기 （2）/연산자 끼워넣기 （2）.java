46779f2c1749f9d77d4faa4f7ecd81ef78278c73
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2023-05-04 08:58:50 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 108 ms, Memory: 14972 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, min, max;
+	static int[] nums, opers;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(br.readLine());
+		
+		nums = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		opers = new int[4];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<4; i++) {
+			opers[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		min = Integer.MAX_VALUE;
+		max = Integer.MIN_VALUE;
+		perm(0, nums[0]);
+		sb.append(max).append("\n").append(min);
+		System.out.println(sb);
+		br.close();
+	}
+	
+	static void perm(int cnt, int res) {
+		if(cnt == N-1) {
+			min = Math.min(min, res);
+			max = Math.max(max, res);
+			
+			return;
+		}
+		
+		for(int i=0; i<4; i++) {
+			if(opers[i] == 0) continue;
+			
+			opers[i]--; // 선택
+			if(i==0) perm(cnt+1, res+nums[cnt+1]); // 덧셈
+			else if(i==1) perm(cnt+1, res-nums[cnt+1]); // 뺄셈
+			else if(i==2) perm(cnt+1, res*nums[cnt+1]); // 곱셈
+			else { // 나눗셈
+				if(res < 0) perm(cnt+1, -(Math.abs(res)/nums[cnt+1]));
+				else perm(cnt+1, res/nums[cnt+1]);
+			}
+			opers[i]++; // 선택 복구
+		}
+	}
+}
