09d0022f08fdd834dad39ddcae4a12fcd4016212
KimSeonHui <zero9657@naver.com>
2023-05-09 21:29:14 +0900

    add B5545 최고의 피자

---
+++ BaekJoon/B5545 최고의 피자/B5545.java
@@ -0,0 +1,40 @@
+package M05_2;
+
+import java.io.*;
+import java.util.*;
+
+public class B5545 {
+
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Integer[] topings = new Integer[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] prices = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+		int calDo = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			topings[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(topings, (o1, o2) -> -Integer.compare(o1, o2));
+		
+		int topingCnt=0;
+		int totalCal = calDo;
+		int perCal = calDo / prices[0];
+		for(int i=0; i<N; i++) {
+			totalCal += topings[i];
+			topingCnt++;
+			
+			int newPerCal = totalCal / (prices[0] + prices[1] * topingCnt);
+			if(perCal <= newPerCal) perCal = newPerCal;
+			else break;
+		}
+		System.out.println(perCal);
+
+		br.close();
+	}
+
+}
