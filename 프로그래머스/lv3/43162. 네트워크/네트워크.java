987eac1aeb411ac6d9c92a8c5a77521aed9839b0
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2023-06-14 17:40:48 +0900

    Update 네트워크.java

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -2,40 +2,38 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    static boolean[][] v;
+    static int[] p;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        v = new boolean[n][n];
+        int answer = n;
+        
+        makeSet(n);
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(!v[i][j] && computers[i][j] == 1) {
-                    bfs(i, j, computers);
-                    answer++;
+                if(i != j && computers[i][j] == 1) {
+                    if(union(i, j)) answer--;
                 }
             }
         }
         return answer;
     }
     
-    public void bfs(int r, int c, int[][] computers) {
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        v[r][c] = true;
-        q.offer(new int[]{r,c});
+    public void makeSet(int n) {
+        p = new int[n];
+        for(int i=0; i<n; i++) p[i] = i;
+    }
+    
+    public int find(int a) {
+        if(p[a] == a) return a;
+        return p[a] = find(p[a]);
+    }
+    
+    public boolean union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
         
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int from = cur[0];
-            int to = cur[1];
-            
-           // System.out.println("from " + from + " to "  + to);
-            
-            for(int i=0; i<computers.length; i++) {
-                if(!v[to][i] && computers[to][i] == 1) {
-                    v[to][i] = true;
-                    q.offer(new int[]{to, i});
-                }
-            }
-        }
+        if(rootA == rootB) return false;
+        p[rootB] = rootA;
+        return true;
     }
 }
