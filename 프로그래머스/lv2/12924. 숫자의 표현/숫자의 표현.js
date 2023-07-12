3bdcb95dab12ab3920df984ca20bed77d0a2969b
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2023-07-12 22:47:55 +0900

    [level 2] Title: 숫자의 표현, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,16 +1,11 @@
 function solution(n) {
     let answer = 0;
     
-    for(let i=1; i<=n; i++) {
-        let sum = 0;
-            for(let j=i; j<=n; j++) {
-                sum += j;
-                if(sum == n) {
-                    answer++;
-                    break;
-            }
-            else if(sum > n) break;
-        }
+    let i = 0;
+    while(n > 0) {
+        i++;
+        if(n % i == 0) answer++;
+        n -= i;
     }
     return answer;
 }
