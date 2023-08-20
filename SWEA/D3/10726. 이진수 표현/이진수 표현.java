fb383ffecb7421343063730d37aa94bdf9fc34e6
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2023-08-20 22:54:57 +0900

    [D3] Title: 이진수 표현, Time: 141 ms, Memory: 23,356 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -15,14 +15,11 @@ public class Solution {
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            boolean flag = true;
-            for(int i=0; i<N; i++) {
-                if((M & (1 << i))  == 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) sb.append("#" + t + " ON" + "\n");
+            int mask = (1 << N) - 1; // 끝 N개 비트를 1인 마스크 생성
+            M &= mask; // M과 mask를 & 연산 >> M의 끝 N개 비트가 유지됨
+
+            // mask가 이미 끝 N개 비트가 1이기 때문에 M과 mask가 같으면 모두 인 비트
+            if(M == mask) sb.append("#" + t + " ON" + "\n");
             else sb.append("#" + t + " OFF" + "\n");
         }
         System.out.println(sb);
