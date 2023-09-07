533f126e47145cb42c55344142df4c1641c783ea
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2023-09-07 22:32:46 +0900

    [level 3] Title: 가장 먼 노드, Time: 41.35 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -11,52 +11,50 @@ class Solution {
         }
     }
     
-    List<Vertex>[] g;
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        
-        g = new List[n+1];
+        // 그래프 만들기
+        List<Vertex>[] g = new List[n+1];
         int[] dist = new int[n+1];
+        
         for(int i=1; i<=n; i++) {
             g[i] = new ArrayList<>();
-            dist[i] = Integer.MAX_VALUE / 2;
+            dist[i] = Integer.MAX_VALUE/2;
         }
+        
         for(int i=0; i<edge.length; i++) {
             int from = edge[i][0];
             int to = edge[i][1];
             
+            // 양방향그래프
             g[from].add(new Vertex(to, 1));
             g[to].add(new Vertex(from, 1));
         }
         
+        boolean[] v = new boolean[n+1];
         PriorityQueue<Vertex> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.weight, o2.weight));
         pq.offer(new Vertex(1, 0));
         dist[1] = 0;
         
-        boolean[] v = new boolean[n+1];
         while(!pq.isEmpty()) {
             Vertex cur = pq.poll();
             int minVertex = cur.to;
             int min = cur.weight;
             
             if(v[minVertex]) continue;
-            
             v[minVertex] = true;
+            
             for(Vertex vertex : g[minVertex]) {
                 if(!v[vertex.to] && dist[vertex.to] > min + vertex.weight) {
-                    dist[vertex.to] = min + vertex.weight;
+                    dist[vertex.to] = cur.weight + vertex.weight;
                     pq.offer(new Vertex(vertex.to, dist[vertex.to]));
                 }
             }
         }
         
-        //System.out.println(Arrays.toString(dist));
-        int max = dist[1];
-        for(int i=2; i<=n; i++) {
-            if(max < dist[i]) max = dist[i];
-        }
-        
-        for(int i=1; i<=n; i++) {
+        Arrays.sort(dist);
+        int max = dist[n];
+        int answer = 1;
+        for(int i=n-1; i>=1; i--) {
             if(max == dist[i]) answer++;
         }
         
