492c30719c514e7895572db9a96e0dc2ee96a6dd
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2023-09-07 23:07:31 +0900

    [level 3] Title: 순위, Time: 19.43 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.java
+++ 프로그래머스/lv3/49191. 순위/순위.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] results) {
-        int answer = 0;
+        // 그래프 만들기
+        int[][] g = new int[n+1][n+1];
         
         // 그래프 초기화
-        int[][] g = new int[n+1][n+1];
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
                 if(i == j) g[i][j] = 0;
@@ -14,15 +14,16 @@ class Solution {
             }
         }
         
-        // 그래프 연결
+        // 결과 정보 추가하기
         for(int i=0; i<results.length; i++) {
-            int to = results[i][0];
-            int from = results[i][1];
+            int win = results[i][0];
+            int lose = results[i][1];
             
-            g[from][to] = 1; // 유향
+            g[lose][win] = 1; // 유향 그래프
         }
         
-        //플로이드워샬
+        
+        // 플로이드워샬
         for(int k=1; k<=n; k++) {
             for(int i=1; i<=n; i++) {
                 for(int j=1; j<=n; j++) {
@@ -31,16 +32,16 @@ class Solution {
             }
         }
         
-       // for(int[] a : g) System.out.println(Arrays.toString(a));
+        int answer = 0;
         for(int i=1; i<=n; i++) {
             int cnt = 0;
             for(int j=1; j<=n; j++) {
-                if(g[i][j] != 0 && g[i][j] < Integer.MAX_VALUE / 2) cnt++; // 진출
-                if(g[j][i] != 0 && g[j][i] < Integer.MAX_VALUE / 2) cnt++; // 진입
+                if(g[i][j] != 0 && g[i][j] < Integer.MAX_VALUE / 2) cnt++;
+                if(g[j][i] != 0 && g[j][i] < Integer.MAX_VALUE / 2) cnt++;
             }
-            //System.out.println(i + " " + cnt);
             if(cnt == n-1) answer++;
         }
+        
         return answer;
     }
 }
