fe185eaf4490b77c03ddb78c6438b603371f8ecb
KimSeonHui <44824456+KimSeonHui@users.noreply.github.com>
2023-09-10 23:30:53 +0900

    [Silver I] Title: 곱셈, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws  Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(sol(a, b, c));
+    }
+
+    public static long sol(long a, long b, long c) {
+        if(b == 1) return a % c;
+        long val = sol(a, b/2, c);
+        val = val * val % c;
+        if(b % 2 == 0) return val;
+        return val * a % c;
+    }
+}
